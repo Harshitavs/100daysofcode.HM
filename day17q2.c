7d5ddd50af18a681332ef9cef46313c9015a157b
Harshita Mathur <mail2harshitamathur@gmail.com>
2025-09-24 01:39:02 +0530

    Add files via upload

---
+++ day17q2.c
@@ -0,0 +1,24 @@
+//Q34: Write a program to check if a number is prime.
+#include <stdio.h>
+#include <stdbool.h>
+int main() {
+    int num;
+    bool isPrime = true;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    if (num <= 1) {
+        isPrime = false;
+    } else {
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+    }
+    if (isPrime)
+        printf("Prime\n");
+    else
+        printf("Not prime\n");
+    return 0;
+}
